e0a095e6f8c636252bd2bc42915f6fe712b594aa
William Chao <wcha609@en-337518.uoa.auckland.ac.nz>
2018-06-01 18:13:57 +1200

    Integrated system with initial testing.

--- src/cruisecontrol.h
+++ src/cruisecontrol.h
@@ -1,2 +1,5 @@
+#include <stdbool.h>
+#define boolean bool
+
 float saturateThrottle(float throttleIn, bool* saturate);
 float regulateThrottle(bool isGoingOn, float cruiseSpeed, float vehicleSpeed);
